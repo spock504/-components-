294f7befbea8ba0569a0e5f697d0f457632051d7
123697@qq.com <123697@qq.com>
2019-01-04 19:59:45 +0800

    feat:coupon component.

---
+++ coupon_comp/components/coupon/coupon.js
@@ -0,0 +1,27 @@
+// components/calendar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    couponsList:{
+      type:Array
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+  attached() {
+    // 在组件实例进入页面节点树时执行
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+   
+  }
+})
